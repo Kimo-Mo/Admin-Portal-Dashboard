098d503fb258c5b81f28829e3274eb81185b19cc
freygold <ahmedtawfik833@gmail.com>
2025-09-12 21:18:04 +0300

    idk: yeah man should

--- src/components/dashboard/supportTickets/cardView/TicketsTagSelector.tsx
+++ src/components/dashboard/supportTickets/cardView/TicketsTagSelector.tsx
@@ -6,14 +6,20 @@ function TicketsTagSelector({ values }: { values: string[] }) {
     label: <span>{item}</span>,
   }));
   return (
-    <Select
-      options={selectionOptions}
-      defaultValue={selectionOptions[0].value}
-      style={{
-        height: '2.3rem',
-        borderRadius: '0.75rem',
-      }}
-    />
+    <div className="">
+      {/* // use state and onChange to handle the selection */}
+      <Select
+        options={selectionOptions}
+        defaultValue={selectionOptions[0].value}
+        style={{
+          height: '2.3rem',
+          borderRadius: '0.75rem',
+          backgroundColor: '#fff',
+        }}
+        // search for this method
+        className="[>_.ant-select-selector]:bg-amber-700"
+      />
+    </div>
   );
 }
 
